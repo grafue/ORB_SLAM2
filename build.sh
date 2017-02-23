006f27f3a2012e519f0d75c2e8922fcd91f4e607
Sele <s-h-schaefer@web.de>
2017-02-23 11:24:58 +0100

    changed build.sh to delete build folders before building

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 echo "Configuring and building Thirdparty/DBoW2 ..."
 
 cd Thirdparty/DBoW2
+rm -r build
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
@@ -10,6 +11,7 @@ cd ../../g2o
 
 echo "Configuring and building Thirdparty/g2o ..."
 
+rm -r build
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
@@ -25,6 +27,7 @@ cd ..
 
 echo "Configuring and building ORB_SLAM2 ..."
 
+rm -r build
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
